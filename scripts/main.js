ded0bcf42c0de1b62016e035c5274486abb13938
ljiang3 <38668453+ljiang3@users.noreply.github.com>
2023-09-02 22:09:25 -0700

    add hw2

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// const btns = document.querySelectorAll(".nav-btn");
+
+// for (let i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function() {
+//       var current = document.getElementsByClassName("active");
+//       current[0].className = current[0].className.replace(" active", "");
+//       this.className += " active";
+//     });
+//   }
