5709fea0cc68c3cc33f580b153007aa509e6ed53
ljiang3 <38668453+ljiang3@users.noreply.github.com>
2023-09-25 14:56:24 -0700

    Delete CNIT 133 directory

--- CNIT 133/scripts/main.js
+++
@@ -1,9 +0,0 @@
-// const btns = document.querySelectorAll(".nav-btn");
-
-// for (let i = 0; i < btns.length; i++) {
-//     btns[i].addEventListener("click", function() {
-//       var current = document.getElementsByClassName("active");
-//       current[0].className = current[0].className.replace(" active", "");
-//       this.className += " active";
-//     });
-//   }
